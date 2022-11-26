d56e737640f304199363159e80c1968b46fd7d7c
dimachilaia <chilaiadima1@gmail.com>
2022-11-26 17:14:48 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+function App() {
+  type Person = {
+
+  }
+  let person:Person = {
+    name:'Dima',
+    age:25
+  }
+  
+  return (
+    <div className="App">
+       
+    </div>
+  );
+}
+
+export default App;
