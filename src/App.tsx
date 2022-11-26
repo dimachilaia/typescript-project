72f20b84e6a55d36eda37c2219d1666e0e24cac1
dimachilaia <chilaiadima1@gmail.com>
2022-11-26 22:29:30 +0400

    added settodo properties

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
+import React, { useState } from "react";
+import InputField from "./components/InputField";
+import { Todo } from "./model";
 
-function App() {
-  type Person = {
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([])
 
+  const handleAdd = (e:React.FormEvent)=>{
+    e.preventDefault()
+    if(todo){
+      setTodos([...todos, {id:Date.now(), todo, isDone:false}]);
+      setTodo('')
+    }
   }
-  let person:Person = {
-    name:'Dima',
-    age:25
-  }
-  
-  return (
+
+  return ( 
     <div className="App">
-       
+      <span className="heading">Taskify</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd }/>
     </div>
   );
-}
+};
 
 export default App;
