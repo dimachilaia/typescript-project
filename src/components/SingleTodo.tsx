841123134ab0d0d1834dd909d4897d538811ee70
dimachilaia <chilaiadima1@gmail.com>
2022-11-27 20:25:38 +0400

    finished

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Todo } from "../model";
 import "./stylecomponent.css";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
@@ -12,7 +12,7 @@ interface Props {
 const SingleTodo: React.FC<Props> = ({ todo, setTodos, todos }) => {
   const [edit, setEdit] = useState<boolean>(false);
   const [editTodo, setEditTodo] = useState<string>(todo.todo);
-  console.log(editTodo);
+  const inputRef = useRef<HTMLInputElement>(null)
 
   const handleDone = (id: number) => {
     setTodos(
@@ -33,6 +33,11 @@ const SingleTodo: React.FC<Props> = ({ todo, setTodos, todos }) => {
     );
     setEdit(false);
   };
+
+  useEffect(()=>{
+    inputRef.current?.focus()
+  },[edit]);
+
   return (
     <form className="todos__single" onSubmit={(e) => handleEdit(e, todo.id)}>
       {edit ? (
@@ -40,6 +45,7 @@ const SingleTodo: React.FC<Props> = ({ todo, setTodos, todos }) => {
           value={editTodo}
           onChange={(e) => setEditTodo(e.target.value)}
           className="todos__single--test"
+          ref={inputRef}
         />
       ) : todo.isDone ? (
         <s className="todos__single--text">{todo.todo}</s>
